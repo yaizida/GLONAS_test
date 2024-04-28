4abe94d06d591c7a2f4dbac55f0185ef27e8fe7c
izida <myizida@gmail.com>
2024-04-28 17:25:48 +0300

    7.5/8

--- task.py
+++ task.py
@@ -44,13 +44,17 @@ class VehicleManager:
         requests.delete(self.url + '/' + str(id))
 
     def distance(self, coordinates1: tuple, coordinates2: tuple) -> int:
-        lon1, lat1, lon2, lat2 = map(math.radians, [coordinates1[0],coordinates1[1],
-                                                    coordinates2[0],coordinates2[1]]
+        lon1, lat1, lon2, lat2 = map(math.radians, [coordinates1[0],
+                                                    coordinates1[1],
+                                                    coordinates2[0],
+                                                    coordinates2[1]
+                                                    ]
                                      )
         # haversine
         dlon = lon2 - lon1
         dlat = lat2 - lat1
-        a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+        a = (math.sin(dlat/2)**2 + math.cos(lat1) *
+             math.cos(lat2) * math.sin(dlon/2)**2)
         c = 2 * math.asin(math.sqrt(a))
         # Радиус Земли в километрах - равен 6371
         dist = 6371000 * c
@@ -60,18 +64,20 @@ class VehicleManager:
         all_vehicles = requests.get(self.url).json()
         list_coordinates = []
         for i in all_vehicles:
-                list_coordinates.append((i['id'], i['latitude'], i['longitude']))
+            list_coordinates.append((i['id'], i['latitude'], i['longitude']))
         return list_coordinates
-
-
 
     def nearest_vehicle(self, id: int) -> str:
         vechile = requests.get(self.url + '/' + str(id))
         coordinates = vechile.json()['latitude'], vechile.json()['longitude']
-
+        nearest_distance = float('inf')
+        nearest_index = -1
+        for index, (machine_lon, machine_lat) in enumerate(self.get_all_coordinates()):
+            distance = self.distance()
+            if distance < nearest_distance:
+                nearest_distance = distance
+                nearest_index = index
         return coordinates
-
-
 
 
 
@@ -81,4 +87,5 @@ vechile = VehicleManager()
 print(vechile.get_vehicles())
 print(vechile.filter_vehicles(params={'name': 'BMW'}))
 print(vechile.get_vehicle(5))
-# print(vechile.nearest_vehicle(5))
+print(vechile.get_all_coordinates())
+print(vechile.nearest_vehicle(5))
